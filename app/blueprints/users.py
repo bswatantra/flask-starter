2151534b76dc6155eb95dc80240aefb0a1e66e7f
swatantra.ch00000 <swatantra.ch@metacloud.com.np>
2023-09-09 22:33:19 +0545

    delete

--- app/blueprints/users.py
+++ app/blueprints/users.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, render_template, redirect, url_for
 from app.models.user import User as UserModel
+from app.extentions import db
 
 bp = Blueprint(
     'users',
@@ -8,17 +9,41 @@ bp = Blueprint(
 )
 
 
-@bp.route("", methods=["GET", "POST"])
+@bp.route("")
 def index():
-    if request.method == "POST":
-        # store user data in db
-        user = request.form
-
-        return redirect(url_for('users.index'))
     users = UserModel.query.all()
     return render_template('user/index.html', users=users)
 
 
-@bp.route("<int:id>")
+@bp.route("create")
+def create():
+    return render_template('user/create.html')
+
+
+@bp.route('', methods=["POST"])
+def store():
+    user = UserModel(
+        name=request.form["name"],
+        email=request.form["email"]
+    )
+    db.session.add(user)
+
+    db.session.commit()
+
+
+@bp.route("<int:id>", methods=["GET"])
 def show(id):
     return render_template('user/show.html', id=id)
+
+
+@bp.route("<int:id>", methods=["PUT"])
+def update(id):
+    return render_template('user/show.html', id=id)
+
+
+@bp.route("<int:id>", methods=["DELETE"])
+def destroy(id):
+    user = UserModel.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for('users.index'))
